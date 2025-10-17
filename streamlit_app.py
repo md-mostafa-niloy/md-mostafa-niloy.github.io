904ed0832137419b6c7064a7dac8340d2df0c36d
MD Mostafa Niloy <mostafa@visernic.com>
2025-10-17 21:45:16 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+# পেজের কনফিগ
+st.set_page_config(page_title="MD Mostafa Niloy - CEO", layout="wide")
+
+# HTML ফাইল ওপেন করে পড়া
+with open("index.html", "r", encoding="utf-8") as f:
+    html_data = f.read()
+
+# HTML কনটেন্ট রেন্ডার করা
+st.components.v1.html(html_data, height=3000, scrolling=True)
 
